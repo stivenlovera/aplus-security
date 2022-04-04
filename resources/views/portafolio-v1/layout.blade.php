c4061755e23f953da03e9edf41b7c0d84653e3c3
stivenlovera <stivenlovera@gmail.com>
2022-04-04 10:51:08 -0400

    pixel elimiando

--- resources/views/portafolio-v1/layout.blade.php
+++ resources/views/portafolio-v1/layout.blade.php
@@ -72,49 +72,7 @@
             ZtVirtuemarter.countdown(0);
         });
     </script>
-    <!-- built with zo2 framework: http://www.zootemplate.com/zo2 -->
 
-    <!-- Meta Pixel Code -->
-    <script>
-        ! function(f, b, e, v, n, t, s) {
-            if (f.fbq) return;
-            n = f.fbq = function() {
-                n.callMethod ?
-                    n.callMethod.apply(n, arguments) : n.queue.push(arguments)
-            };
-            if (!f._fbq) f._fbq = n;
-            n.push = n;
-            n.loaded = !0;
-            n.version = '2.0';
-            n.queue = [];
-            t = b.createElement(e);
-            t.async = !0;
-            t.src = v;
-            s = b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t, s)
-        }(window, document, 'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-        fbq('init', '837688500965980');
-        fbq('track', 'PageView');
-    </script>
-    <noscript><img height="1" width="1" style="display:none"
-            src="https://www.facebook.com/tr?id=837688500965980&ev=PageView&noscript=1" /></noscript>
-    <!-- End Meta Pixel Code -->
-    
-
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-208227553-1">
-    </script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-
-        function gtag() {
-            dataLayer.push(arguments);
-        }
-        gtag('js', new Date());
-
-        gtag('config', 'UA-208227553-1');
-    </script>
     <meta name="facebook-domain-verification" content="isz0fv9qipgisay1m621wkhbt4o1gt" />
     <link href="{{ asset('portafolio-v1/favicon.ico') }}" rel="shortcut icon" type="image/vnd.microsoft.icon" />
     <link rel="stylesheet" href="{{ asset('portafolio-v1/css/bootstrap.min.css') }}">
